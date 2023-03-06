9f8170b6b5a842b3ebeeaf85936a0969f4231c0f
pizzalicer <alicerodrigueslili.05@gmail.com>
2023-03-06 10:40:31 -0300

    oi

---
+++ src/components/ListMarker/index.tsx
@@ -0,0 +1,6 @@
+import { Text, View } from "react-native";
+import { Marker } from '../Marker'
+import { styles } from './styles'
+export interface ITextMarker {
+    textMarker: string
+}
---
+++ src/styles/colors.ts
@@ -0,0 +1,8 @@
+export const colors = {
+    primary: '#FA9BC8',
+    secondary: '#EDB5CF',
+    third: '#D9D9D9',
+    thirdLigth: 'rgba(217,217,217, 0.8)',
+    black: '#000',
+    white: '#fff',
+}
